4c7e0b47a50bbebbc8973b09928311eff6a456b2
GeovaniSS <geosilvasousa@gmail.com>
2022-06-09 23:37:21 -0300

    [feat]: Location links to Repos page, Followers page and Following page | Dynamic imports | User login saved in localStorage

--- src/modules/searchGitHubUser.js
+++ src/modules/searchGitHubUser.js
@@ -1,21 +1,55 @@
 import { fetchGitHubUser } from './fetchGitHubUser';
-import { fetchGitHubUserRepos } from './fetchGitHubUserRepos';
-import { fetchGitHubUserFollowers } from './fetchGitHubUserFollowers';
-import { fetchGitHubUserFollowing } from './fetchGitHubUserFollowing';
-// import { reposUrl, followingUrl, followersUrl } from './renderGitHubUser'
+fetchGitHubUser(localStorage.getItem('user') || 'GeovaniSS')
 
 const userSearch = document.querySelector('#user-search')
 const btnSearch = document.querySelector('.button-search')
 
+const repos = document.querySelector('.profile-repos')
+const followers = document.querySelector('.profile-followers')
+const following = document.querySelector('.profile-following')
+
 export default () => {
-  btnSearch.addEventListener('click', () => fetchGitHubUser(userSearch.value))
+  btnSearch.addEventListener('click', () => { 
+    fetchGitHubUser(userSearch.value)
+    localStorage.setItem('user', userSearch.value)
+  })
+
   userSearch.addEventListener('keypress', (e) => {
     if(e.keyCode === 13) fetchGitHubUser(userSearch.value)
+    localStorage.setItem('user', userSearch.value)
   })
+
   userSearch.addEventListener('change', () => document.querySelector('#no-results').style.display = 'none')
+  
+  repos.addEventListener('click', () => location.assign(location.origin + '/public/pages/repos.html'))
+  followers.addEventListener('click', () => location.assign(location.origin + '/public/pages/followers.html'))
+  following.addEventListener('click', () => location.assign(location.origin + '/public/pages/following.html')) 
 }
 
-fetchGitHubUser('GeovaniSS')
-fetchGitHubUserRepos('https://api.github.com/users/GeovaniSS/repos')
-fetchGitHubUserFollowers('https://api.github.com/users/GeovaniSS/followers')
-fetchGitHubUserFollowing('https://api.github.com/users/GeovaniSS/following')
+const loadUserReposData = () => {
+  if(location.pathname === '/public/pages/repos.html') {
+    import('./fetchGitHubUserRepos')
+      .then(({ fetchGitHubUserRepos }) => fetchGitHubUserRepos(localStorage.getItem('user')))
+      .catch(e => console.log(e))
+  }
+}
+
+const loadUserFollowersData = () => {
+  if(location.pathname === '/public/pages/followers.html') {
+    import('./fetchGitHubUserFollowers')
+    .then(({ fetchGitHubUserFollowers }) => fetchGitHubUserFollowers(localStorage.getItem('user')))
+    .catch(e => console.log(e))
+  }
+}
+
+const loadUserFollowingData = () => {
+  if(location.pathname === '/public/pages/following.html') {
+    import('./fetchGitHubUserFollowing')
+      .then(({ fetchGitHubUserFollowing }) => fetchGitHubUserFollowing(localStorage.getItem('user')))
+      .catch(e => console.log(e))
+  }
+}
+
+loadUserReposData()
+loadUserFollowersData()
+loadUserFollowingData()
