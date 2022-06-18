66790fc781ee84740079a9e76421ea18c825aa0d
GeovaniSS <geosilvasousa@gmail.com>
2022-06-17 22:19:51 -0300

    [fix]: repo html structure

--- src/modules/renderGitHubUserRepos.js
+++ src/modules/renderGitHubUserRepos.js
@@ -6,16 +6,18 @@ export const renderGitHubUserRepos = gitHubUserRepos => {
     return acc + ` 
     <div class="repo">
       <div class="repo-info">
-        <a href="${html_url}"> <h2 id="repos-name">${name}</h2> </a>
-        <p id="repos-description">${description === null ? 'No description' : description}</p>
-      </div>
-
-      <div class="repo-star">
-        <div class="star">
-          <span class="material-icons-outlined">star</span>
-          <span id="total-stars">${stargazers_count}</span>
+        <a href="${html_url}" target="_blank">
+          <h2 id="repos-name">${name}</h2> 
+        </a>
+        <div class="repo-star">
+          <div class="star">
+            <span class="material-icons-outlined">star</span>
+            <span id="total-stars">${stargazers_count}</span>
+          </div>
         </div>
       </div>
+
+      <p id="repo-description">${description === null ? 'No description' : description}</p>
     </div>
     `
   }, '')
