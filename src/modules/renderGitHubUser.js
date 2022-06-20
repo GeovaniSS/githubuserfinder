2a2d93946305ac9fe3c10127d7f91d2ac5f79cc6
GeovaniSS <geosilvasousa@gmail.com>
2022-06-19 23:16:18 -0300

    [fix]: render github user profile and check if is null

--- src/modules/renderGitHubUser.js
+++ src/modules/renderGitHubUser.js
@@ -3,18 +3,18 @@ export const renderGitHubUser = gitHubUser => {
   following, blog, company, twitter_username, location } = gitHubUser
   
   document.querySelector('.profile-photo').src = avatar_url
-  document.querySelector('#name').innerText = name === null ? login : name
+  document.querySelector('#name').innerText = !name ? login : name
   document.querySelector('#login').innerText = `@${login.toLowerCase()}`
   
   if(!document.querySelector('#bio')) return
   
-  document.querySelector('#bio').innerText = bio === null ? 'This profile has no bio': bio
+  document.querySelector('#bio').innerText = !bio ? 'This profile has no bio': bio
   document.querySelector('#repos').innerText = public_repos
   document.querySelector('#followers').innerText = followers
   document.querySelector('#following').innerText = following
-  document.querySelector('#blog').innerText = blog === null ? 'No Blog': blog
-  document.querySelector('#company').innerText = company === null ? 'No Company': company 
-  document.querySelector('#location').innerText = location === null ? 'No Location': location
-  document.querySelector('#twitter').innerText = twitter_username === null ? 'No Twitter': twitter_username
+  document.querySelector('#blog').innerText = !blog ? 'No Blog': blog
+  document.querySelector('#company').innerText = !company ? 'No Company': company 
+  document.querySelector('#location').innerText = !location ? 'No Location': location
+  document.querySelector('#twitter').innerText = !twitter_username ? 'No Twitter': twitter_username
 }
 
