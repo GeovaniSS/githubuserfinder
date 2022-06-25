c8e0d7d890eb6ea6c192acb6474261414a113d66
Geovani Silva <90160888+GeovaniSS@users.noreply.github.com>
2022-06-24 23:59:54 -0300

    Delete showEmptyMessage.js

--- src/modules/showEmptyMessage.js
+++
@@ -1,7 +0,0 @@
-export const showEmptyMessage = (msg, parent) => {
-  const emptyMessage = document.createElement('h1')
-  emptyMessage.classList.add('error')
-  emptyMessage.innerText = msg
-  
-  parent.appendChild(emptyMessage)
-}
