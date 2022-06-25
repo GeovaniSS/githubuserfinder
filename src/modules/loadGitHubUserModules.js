ee6fb5d854bbe020ec281ffbbe13dc9e7f4d9705
GeovaniSS <geosilvasousa@gmail.com>
2022-06-24 23:49:46 -0300

    🐛 fix: modules path

--- src/modules/loadGitHubUserModules.js
+++ src/modules/loadGitHubUserModules.js
@@ -1,4 +1,4 @@
-import { searchGitHubUser } from './searchGitHubUser';
+import searchGitHubUser from './user/searchGitHubUser';
 
 const user = localStorage.getItem('user')
 
@@ -8,22 +8,22 @@ export default async() => {
   const url = location.href
 
   if(url.includes('index.html')) { 
-    const { default: gitHubUserEvents } = await import('./gitHubUserEvents')
+    const { default: gitHubUserEvents } = await import('./user/gitHubUserEvents')
     gitHubUserEvents()
   }
 
   if(url.includes('repos.html')) {
-    const { searchGitHubUserRepos } = await import('./searchGitHubUserRepos')
+    const { searchGitHubUserRepos } = await import('./repos/searchGitHubUserRepos')
     searchGitHubUserRepos(user)
   }
 
   if(url.includes('followers.html')) {
-    const { searchGitHubUserFollowers } = await import('./searchGitHubUserFollowers')
+    const { searchGitHubUserFollowers } = await import('./followers/searchGitHubUserFollowers')
     searchGitHubUserFollowers(user)
   }
 
   if(url.includes('following.html')) {
-    const { searchGitHubUserFollowing } = await import('./searchGitHubUserFollowing')
+    const { searchGitHubUserFollowing } = await import('./following/searchGitHubUserFollowing')
     searchGitHubUserFollowing(user)
   }
 }
