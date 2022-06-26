425e245e10d2671c9f653b5fcf5248b367dc2182
GeovaniSS <geosilvasousa@gmail.com>
2022-06-26 19:27:08 -0300

    ✨ feat: clear userSearchInput value

--- src/modules/user/gitHubUserEvents.js
+++ src/modules/user/gitHubUserEvents.js
@@ -8,9 +8,15 @@ const noResultsMsg = document.querySelector('#no-results')
 const hideNoResultsMsg = () => noResultsMsg.style.display = 'none'
 
 export default () => {
-  btnSearch.addEventListener('click', () => searchGitHubUser(userSearchInput.value))
+  btnSearch.addEventListener('click', () => { 
+    searchGitHubUser(userSearchInput.value) 
+    userSearchInput.value = ""
+  })
   userSearchInput.addEventListener('keypress', (e) => {
-    if(e.keyCode === 13) searchGitHubUser(userSearchInput.value)
+    if(e.keyCode === 13) { 
+      searchGitHubUser(userSearchInput.value)
+      userSearchInput.value = ""
+    }
   })
   userSearchInput.addEventListener('change', () => hideNoResultsMsg())
 }
